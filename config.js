2745c0fd8c91e00fd131180544b8e3899de07090
xYAZUWA <92874892+xYAZUWA@users.noreply.github.com>
2021-11-15 18:27:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -69,7 +69,7 @@ module.exports = {
     EMOJI_DESCRIPTION: process.env.EMOJI_DESCRIPTION === undefined ? '⚙️' : process.env.EMOJI_DESCRIPTION,
     EMOJI_EXAMPLE: process.env.EMOJI_EXAMPLE === undefined ? '💬' : process.env.EMOJI_EXAMPLE,
     EMOJI_WARNING: process.env.EMOJI_WARNING === undefined ? '⚠️' : process.env.EMOJI_WARNING,
-    PANEL_COMMAND: process.env.PANEL_COMMAND === undefined ? 'alexa' : process.env.PANEL_COMMAND,
+    PANEL_COMMAND: process.env.PANEL_COMMAND === undefined ? 'dug' : process.env.PANEL_COMMAND,
     BUG_COMMAND: process.env.BUG_COMMAND === undefined ? 'bug' : process.env.BUG_COMMAND,
     TIME_ZONE: process.env.TIME_ZONE === undefined ? 'Asia/Colombo' : process.env.TIME_ZONE,
     HTTPS: process.env.HTTPS === undefined ? 'https://' : process.env.HTTPS,
